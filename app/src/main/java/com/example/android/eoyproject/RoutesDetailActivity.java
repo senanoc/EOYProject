02427fef9eb0376c1e0f94ad5b54b4d327d872f2
senanoc <senanoc@gmail.com>
2018-12-08 11:23:00 +0000

    Fixed firebase link & tweak RecyclerView

--- app/src/main/java/com/example/android/eoyproject/RoutesDetailActivity.java
+++ app/src/main/java/com/example/android/eoyproject/RoutesDetailActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.eoyproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -33,7 +34,6 @@ public class RoutesDetailActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         mNameTextView = findViewById(R.id.detail_name);
         mAddrTextView = findViewById(R.id.detail_addr);
-
 
 
         String docId = getIntent().getStringExtra(Constants.EXTRA_DOC_ID);
